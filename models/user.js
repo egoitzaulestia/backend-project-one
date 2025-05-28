992a4901de470dd5d99d818d7f1602a64ee569d3
egoitzaulestia <egoitzaulestia@gmail.com>
2025-05-29 01:49:15 +0200

    user model little change

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.belongsTo(models.Role);
+      User.belongsTo(models.Role, { foreignKey: 'RoleId' });
       User.hasMany(models.Order);
       User.hasMany(models.Review);
     }
