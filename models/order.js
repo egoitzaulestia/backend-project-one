98711affc5834eb0bcaa2d2e3f6257911f395b94
egoitzaulestia <egoitzaulestia@gmail.com>
2025-05-28 13:21:22 +0200

    updated order model association

--- models/order.js
+++ models/order.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Order.belongsToMany(models.Product, { through: models.OrderItem });
     }
   }
   Order.init(
