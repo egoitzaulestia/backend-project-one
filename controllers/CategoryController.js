89d0910479366d91656f5b556356bdc018f5f75f
egoitzaulestia <egoitzaulestia@gmail.com>
2025-05-29 21:29:03 +0200

    CategoryController.js and category insert controller created

---
+++ controllers/CategoryController.js
@@ -0,0 +1,14 @@
+const { Category } = require('../models/index');
+
+const CategoryController = {
+  async insert(req, res) {
+    try {
+      const category = await Category.create(req.body);
+      res
+        .status(201)
+        .send({ message: 'Category added successfully', category });
+    } catch (error) {
+      res.status(500).send({ message: 'Error', error });
+    }
+  },
+};
