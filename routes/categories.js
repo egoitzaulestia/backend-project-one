47009015dac673d0613d64e07347d6ac113f91cd
egoitzaulestia <egoitzaulestia@gmail.com>
2025-05-29 21:57:27 +0200

    getall categories route created, and working

--- routes/categories.js
+++ routes/categories.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const CategoryController = require('../controllers/CategoryController');
 
 router.post('/', CategoryController.insert);
+router.get('/', CategoryController.getAll);
 
 module.exports = router;
