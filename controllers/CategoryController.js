7c1e3de063be4d6813555b8c8516d16e2f3c5cea
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-02 13:37:07 +0200

    jwt installed and authentication.js middleware file created

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -56,19 +56,6 @@ const CategoryController = {
     res.status(200).send(category);
   },
 
-  // getOneByTitle(req, res) {
-  //   Post.findOne({
-  //     where: {
-  //       title: {
-  //         [Op.like]: `%${req.params.title}%`,
-  //       },
-  //     },
-  //     include: [User],
-  //   })
-  //     .then((post) => res.status(200).send(post))
-  //     .catch((error) => res.status(500).send({ message: 'Error!', error }));
-  // },
-
   async update(req, res) {
     try {
       const [updated] = await Category.update(req.body, {
