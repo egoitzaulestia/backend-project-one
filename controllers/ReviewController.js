afd19ef6c4a76d2ce2b0acd3a1b968032ad21865
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-02 17:21:17 +0200

    category validation ready

--- controllers/ReviewController.js
+++ controllers/ReviewController.js
@@ -1,2 +1,10 @@
 const { Review, User, Products, Sequelize } = require('../models/index');
 const { Op } = Sequelize;
+
+const ReviewController = {
+  async create(req, res) {
+    try {
+      const review = '';
+    } catch (error) {}
+  },
+};
