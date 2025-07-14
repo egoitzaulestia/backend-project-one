fba33bffa1478f8613c649cdeccf69ead1324cbb
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-14 13:42:53 +0200

    cors intalled

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { typeError } = require('./middlewares/errors');
+const cors = require('cors');
 // const path = require('path');
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
