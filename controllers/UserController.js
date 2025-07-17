8289375957fb4d02f40f56db9b160f41fbc16259
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-18 01:17:51 +0200

    UserController create updated

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,6 +8,7 @@ const {
   Sequelize,
 } = require('../models/index');
 const { Op } = Sequelize;
+const { UniqueConstraintError } = require('sequelize');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { jwt_secret } = require('../config/config')['development'];
@@ -28,14 +29,15 @@ const UserController = {
         ...req.body,
         password: hashedPassword,
         confirmed: false,
-        RoleId: 3, // we initaite every user as 'user' role
+        RoleId: 1, // we initaite every user as 'user' role
       });
 
       const emailToken = jwt.sign({ email: req.body.email }, jwt_secret, {
         expiresIn: '48h',
       });
       // IMPORTANT: Correct the "2" in users2 !!!
-      const url = `http://localhost:3000/users/confirm/${emailToken}`;
+      // const url = `http://localhost:3000/users/confirm/${emailToken}`; // local Backend URL
+      const url = `http://localhost:5173/confirm/${emailToken}`; // local Frontend URL
       await transporter.sendMail({
         to: req.body.email,
         subject: 'Confirm your regist',
@@ -50,7 +52,11 @@ const UserController = {
         user,
       });
     } catch (error) {
-      console.error(error);
+      if (err instanceof UniqueConstraintError) {
+        return res.status(400).send({
+          message: 'That email is already registered. Try logging in instead.',
+        });
+      }
       next(error);
     }
   },
